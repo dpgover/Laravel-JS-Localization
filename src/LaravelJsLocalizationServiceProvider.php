792cf12ae01e4903b003856d5fc9b250b93e1a20
dgovergun <dgovergun@digbang.com>
2014-06-05 17:54:15 -0300

    - path fixes

--- src/LaravelJsLocalizationServiceProvider.php
+++ src/LaravelJsLocalizationServiceProvider.php
@@ -4,35 +4,35 @@ use Illuminate\Support\ServiceProvider;
 
 class LaravelJsLocalizationServiceProvider extends ServiceProvider
 {
-    protected $defer = false;
+	protected $defer = false;
 
-    public function register()
-    {
-        $this->app['localization.js'] = $this->app->share(function ($app) {
-            $generator = new Generators\LangJsGenerator($app['files']);
-            return new Commands\LangJsCommand($generator);
-        });
+	public function register()
+	{
+		$this->app['localization.js'] = $this->app->share(function ($app) {
+			$generator = new Generators\LangJsGenerator($app['files']);
+			return new Commands\LangJsCommand($generator);
+		});
 
-        $this->commands('localization.js');
-    }
+		$this->commands('localization.js');
+	}
 
-    public function provides()
-    {
-        return array('localization.js');
-    }
+	public function provides()
+	{
+		return array('localization.js');
+	}
 
 	public function boot()
 	{
-		$this->package('mariuzzo/js-localization');
+		$this->package('dpgover/js-localization');
 
 		$this->registerNamespaces();
 	}
 
 	private function registerNamespaces()
 	{
-		if (\File::isDirectory(app_path() . '/config/packages/mariuzzo/js-localization'))
+		if (\File::isDirectory(app_path() . '/config/packages/dpgover/laravel-js-localization'))
 		{
-			\Config::addNamespace('js-localization', app_path() . '/config/packages/mariuzzo/js-localization');
+			\Config::addNamespace('js-localization', app_path() . '/config/packages/dpgover/laravel-js-localization');
 		}
 		else
 		{
