e1da3439cf360d3fec0591a25a29c039a7c17d18
dgovergun <dgovergun@digbang.com>
2014-06-05 18:01:25 -0300

    fixed js messages generation

--- src/Generators/LangJsGenerator.php
+++ src/Generators/LangJsGenerator.php
@@ -5,68 +5,70 @@ use JShrink\Minifier;
 
 class LangJsGenerator
 {
-    protected $file;
+	protected $file;
 
-    public function __construct(File $file)
-    {
-        $this->file = $file;
-    }
+	public function __construct(File $file)
+	{
+		$this->file = $file;
+	}
 
-    public function make($target, $options)
-    {
-        $messages = $this->getMessages();
-        $this->prepareTarget($target);
+	public function make($target, $options)
+	{
+		$messages = $this->getMessages();
+		$this->prepareTarget($target);
 
-        $template = $this->file->get(__DIR__ . '/Templates/langjs_with_messages.js');
-        $langjs = $this->file->get(__DIR__ . '/../../js/lang.js');
+		$template = $this->file->get(__DIR__ . '/Templates/langjs_with_messages.js');
+		$langjs = $this->file->get(__DIR__ . '/../../js/lang.js');
 
-        $template = str_replace('\'{ messages }\'', json_encode($messages), $template);
-        $template = str_replace('\'{ langjs }\';', $langjs, $template);
+		$template = str_replace('\'{ messages }\'', json_encode($messages), $template);
+		$template = str_replace('\'{ langjs }\';', $langjs, $template);
 
-        if ($options['compress'])
-        {
-            $template = Minifier::minify($template);
-        }
+		if ($options['compress'])
+		{
+			$template = Minifier::minify($template);
+		}
 
-        return $this->file->put($target, $template);
-    }
+		return $this->file->put($target, $template);
+	}
 
-    protected function getMessages()
-    {
-	    $messages = [];
-	    $path = app_path() . '/lang';
+	protected function getMessages()
+	{
+		$messages = [];
+		$path = app_path() . '/lang';
 
-	    if (!$this->file->exists($path))
-	    {
-		    throw new \Exception("${path} doesn't exists!");
-	    }
+		if (!$this->file->exists($path))
+		{
+			throw new \Exception("${path} doesn't exists!");
+		}
 
-	    $messageKeys = \Config::get('js-localization::config.messages');
+		$messageKeys = \Config::get('js-localization::config.messages');
 
-	    $langs = $this->file->directories($path);
-	    foreach ($langs as $lang)
-	    {
-		    $lang = basename($lang);
-		    foreach($messageKeys as $key)
-		    {
-			    if(\Lang::has($key, $lang))
-			    {
-				    $jsKey = $lang . '.' . $key;
-				    $messages[$jsKey] = \Lang::get($key, [], $lang);
-			    }
-		    }
-	    }
+		$langs = $this->file->directories($path);
+		foreach ($langs as $lang)
+		{
+			$lang = basename($lang);
+			foreach($messageKeys as $key)
+			{
+				if(\Lang::has($key, $lang))
+				{
+					$parsedKey = explode('.', $key, 2);
 
-	    return $messages;
-    }
+					$jsKey = $lang . '.' . $parsedKey[0];
+					$messages[$jsKey][$parsedKey[1]] = \Lang::get($key, [], $lang);
+				}
+			}
+		}
 
-    protected function prepareTarget($target)
-    {
-        $dirname = dirname($target);
+		return $messages;
+	}
 
-        if (!$this->file->exists($dirname))
-        {
-            $this->file->makeDirectory($dirname);
-        }
-    }
+	protected function prepareTarget($target)
+	{
+		$dirname = dirname($target);
+
+		if (!$this->file->exists($dirname))
+		{
+			$this->file->makeDirectory($dirname);
+		}
+	}
 }
